887087db024fdf119baab05fd1e483333f295a79
nari <naringst@gmail.com>
2023-10-30 23:08:27 +0900

    feat : grid 활용 컴포넌트 배치

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,26 @@
 import React from "react";
 
 import "./App.css";
+import Sidebar from "./components/Sidebar";
+import Header from "./components/Header";
+import Home from "./components/Home";
+import { styled } from "styled-components";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Grid className="App">
+      <Sidebar />
+      <Header />
+      <Home />
+    </Grid>
+  );
 }
 
 export default App;
+
+const Grid = styled.div`
+  display: grid;
+  grid-template-areas: "Sidebar Header" "Sidebar Home";
+  grid-template-columns: 300px 1fr;
+  grid-template-rows: 70px 1fr;
+`;
