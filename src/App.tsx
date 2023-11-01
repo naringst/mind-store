5f87efa5c1fb4b5ffbb24efacc6b3d4bf2ba5822
nari <naringst@gmail.com>
2023-11-02 00:01:32 +0900

    feat : +버튼 클릭 시 모달 열기 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,32 @@
-import React from "react";
-
+import { useSelector } from "react-redux/es/hooks/useSelector";
 import "./App.css";
 import Sidebar from "./components/Sidebar";
 import Header from "./components/Header";
 import Home from "./components/Home";
 import { styled } from "styled-components";
+import CreateNoteModal from "./components/CreateNoteModal";
 
 function App() {
+  const isModalOpen = useSelector((state: any) => state.modal.isModalOpen);
+
   return (
-    <Grid className="App">
-      <Sidebar />
-      <Header />
-      <Home />
-    </Grid>
+    <>
+      {isModalOpen && <CreateNoteModal />}
+      <Container className="App">
+        <Sidebar />
+        <Header />
+        <Home />
+      </Container>
+    </>
   );
 }
 
 export default App;
 
-const Grid = styled.div`
-  display: grid;
+const Container = styled.div`
+  display: flex;
   grid-template-areas: "Sidebar Header" "Sidebar Home";
   grid-template-columns: 300px 1fr;
   grid-template-rows: 70px 1fr;
+  z-index: 0;
 `;
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,21 @@
-import React from "react";
 import { styled } from "styled-components";
+import { modalActions } from "../store";
 
-export default function Header() {
-  const title: string = "title";
+import { useDispatch } from "react-redux";
+
+export default function Header({ openModal }: any) {
+  const title: string = "노트";
+  const dispatch = useDispatch();
+  const openModalHandler = (e: any) => {
+    e.preventDefault();
+    dispatch(modalActions.openModal());
+    console.log("opened");
+  };
 
   return (
     <StyledHeader>
       <h1>{title}</h1>
-      <Button>+</Button>
-      <Button>-</Button>
+      <Button onClick={openModalHandler}>+</Button>
     </StyledHeader>
   );
 }
@@ -16,16 +23,19 @@ export default function Header() {
 const StyledHeader = styled.header`
   height: 55px;
   width: 100%;
-  padding: 10px;
-  z-index: -1;
+  padding: 10px 10px 10px 40px;
   grid-area: Header;
   display: flex;
+  justify-content: space-between;
+  align-items: center;
   box-shadow: 0px 10px 5px -2px gray;
 `;
 
 const Button = styled.button`
   width: 30px;
   height: 30px;
+  margin: 10px 50px;
   background: yellow;
   border-radius: 5px;
+  cursor: pointer;
 `;
