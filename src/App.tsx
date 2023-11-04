c6e98203776f56ce130546a4dac7c6ca9791eeef
nari <naringst@gmail.com>
2023-11-04 23:49:08 +0900

    fix : react-router-dom 적용 페이지 렌더링

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,35 @@
 import { useSelector } from "react-redux/es/hooks/useSelector";
 import "./App.css";
-import Sidebar from "./components/Sidebar";
-import Header from "./components/Header";
-import Home from "./components/Home";
+import Home from "./components/Pages/Home";
 import { styled } from "styled-components";
-import CreateNoteModal from "./components/CreateNoteModal";
+import CreateNoteModal from "./components/Modal/CreateNoteModal";
+import TagModal from "./components/Modal/TagModal";
+import { Route, Routes } from "react-router-dom";
+import Layout from "./components/Layout/Layout";
+import Note from "./components/Pages/Note";
+import Archive from "./components/Pages/Archive";
+import Trash from "./components/Pages/Trash";
 
 function App() {
   const isModalOpen = useSelector((state: any) => state.modal.isModalOpen);
+  const isTagModalOpen = useSelector(
+    (state: any) => state.modal.isTagModalOpen
+  );
+  //tags 가져오기
+  const tagName = "note";
 
   return (
-    <>
-      {isModalOpen && <CreateNoteModal />}
-      <Container className="App">
-        <Sidebar />
-        <Header />
-        <Home />
-      </Container>
-    </>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="note" element={<Note />} />
+        <Route path="archive" element={<Archive />} />
+        <Route path="trash" element={<Trash />} />
+        {isModalOpen && <Route path="*" element={<CreateNoteModal />} />}
+        {isTagModalOpen && <Route path="*" element={<TagModal />} />}
+      </Route>
+    </Routes>
   );
 }
 
 export default App;
-
-const Container = styled.div`
-  display: flex;
-  grid-template-areas: "Sidebar Header" "Sidebar Home";
-  grid-template-columns: 300px 1fr;
-  grid-template-rows: 70px 1fr;
-  z-index: 0;
-`;
