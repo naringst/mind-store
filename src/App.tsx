23845b9b8c20f24f2e6870fa148ce4fe4110962c
nari <naringst@gmail.com>
2023-11-13 01:24:56 +0900

    feat : 태그 추가 모달 및 태그 설정 모달 렌더링 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
 import { useSelector } from "react-redux/es/hooks/useSelector";
 import "./App.css";
 import Home from "./components/Pages/Home";
-import { styled } from "styled-components";
 import CreateNoteModal from "./components/Modal/CreateNoteModal";
-import TagModal from "./components/Modal/TagModal";
 import { Route, Routes } from "react-router-dom";
 import Layout from "./components/Layout/Layout";
-import Note from "./components/Pages/Note";
 import Archive from "./components/Pages/Archive";
 import Trash from "./components/Pages/Trash";
 import Tag from "./components/Pages/Tag";
 import { TagType } from "./store";
+
+import AddTagModal from "./components/Modal/AddTagModal";
+import EditTagModal from "./components/Modal/EditTagModal";
 
 function App() {
   const isModalOpen = useSelector((state: any) => state.modal.isModalOpen);
@@ -18,13 +18,18 @@ function App() {
     (state: any) => state.tagModal.isTagModalOpen
   );
 
+  const isAddTagModalOpen = useSelector(
+    (state: any) => state.tagModal.isAddTagModalOpen
+  );
   //tags 가져오기
   const tagList = useSelector((state: any) => state.tag.tagList);
 
   return (
     <>
       {isModalOpen && <CreateNoteModal />}
-      {isTagModalOpen && <TagModal />}
+      {isAddTagModalOpen && <AddTagModal />}
+      {isTagModalOpen && <EditTagModal />}
+
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route path="/" element={<Home />} />
